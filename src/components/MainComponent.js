acce479c388d0694da6f63f9acfeb3e5864b8e59
therealhsk <sheikh.hsk@gmail.com>
2020-01-09 13:12:01 +0500

    Controlled Forms Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,6 +10,7 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import Dish from './DishdetailComponent'
+import About from './AboutComponent'
 
 
 class Main extends Component {
@@ -51,9 +52,9 @@ class Main extends Component {
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
                     <Route exact path='/contactus' component={Contact} />} />
                     <Route path='/menu/:dishId' component={DishWithId} />
+                    <Route path='/aboutus' component={() => <About leaders={this.state.leaders} /> } />
                     <Redirect to="/home" />
                 </Switch>
-
                 <Footer />
             </div>
         )
